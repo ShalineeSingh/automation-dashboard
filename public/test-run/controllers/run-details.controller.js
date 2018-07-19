7709fc6d70f569d6344a06f14e2c235b2ada1a7c
shalineesingh <shalinee.singh@coviam.com>
2018-07-19 11:17:34 +0530

    minor fixes

--- public/test-run/controllers/run-details.controller.js
+++ public/test-run/controllers/run-details.controller.js
@@ -10,88 +10,6 @@
         'status_map': $rootScope.maps.status_map,
         'platform_map': $rootScope.maps.platform_map
       };
-      // $scope.run_details.data = {
-      //   "timeStamp": "11-07-2018 08:48:27",
-      //   "responseObject": {
-      //     "testRunDetails": {
-      //       "id": 53,
-      //       "platform": "WEB",
-      //       "suiteName": "Confidence",
-      //       "release": "R4",
-      //       "total_tests": 70,
-      //       "passed_tests": 2,
-      //       "failed_tests": 0,
-      //       "start_time": "2018-07-09T12:28:28.438Z",
-      //       "end_time": "2018-07-09T14:28:28.438Z",
-      //       "android_pref_id": 54,
-      //       "deviceID": "D1",
-      //       "buildID": "B1",
-      //       "deviceName": "Moto X",
-      //     },
-      //     "testCaseResultDetails": [{
-      //         "id": 55,
-      //         "runID": 53,
-      //         "testGroupName": "G1",
-      //         "testCaseName": "Create Event",
-      //         "testData": "data 1\ndata 2\n data3",
-      //         "testStatus": "PASS",
-      //         "testSteps": "Diary Of a Cat and her Meow adventures\nThe human i live with is an idiot.",
-      //         "errorMessage": "Exception in PaymentPage SelectCreditCardAndCompleteBooking() ::java.lang.Exception: Unable to find page element with Tagname 'orderID'",
-      //         "consoleLogs": "Log 1",
-      //         'screenshot': "data:image/gif;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw=="
-
-      //       },
-      //       {
-      //         "id": 58,
-      //         "runID": 53,
-      //         "testGroupName": "G2",
-      //         "testCaseName": "T1",
-      //         "testStatus": "PASS",
-      //         "errorMessage": "",
-      //         "consoleLogs": "C1",
-      //         "testSteps": "Diary Of a Cat and her Meow adventures",
-      //       },
-      //       {
-      //         "id": 59,
-      //         "runID": 53,
-      //         "testGroupName": "G2",
-      //         "testCaseName": "T1",
-      //         "testStatus": "PASS",
-      //         "errorMessage": "",
-      //         "consoleLogs": "C1"
-      //       },
-      //       {
-      //         "id": 60,
-      //         "runID": 53,
-      //         "testGroupName": "G2",
-      //         "testCaseName": "T1",
-      //         "testStatus": "PASS",
-      //         "testSteps": "[Step 1, Step 2]",
-      //         "errorMessage": "",
-      //         "consoleLogs": "C1",
-      //         "start_time": "2018-07-11T11:13:56.297Z",
-      //         "end_time": "2018-07-11T11:13:57.443Z"
-      //       },
-      //       {
-      //         "id": 61,
-      //         "runID": 53,
-      //         "testGroupName": "G2",
-      //         "testCaseName": "T3",
-      //         "testStatus": "FAIL",
-      //         "testSteps": "[Step 1, Step 2]",
-      //         "errorMessage": "",
-      //         "consoleLogs": "C1",
-      //         "start_time": "2018-07-11T12:15:40.046Z",
-      //         "end_time": "2018-07-11T12:15:41.261Z",
-      //         'screenshot': "data:image/gif;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw=="
-
-      //       }
-      //     ]
-      //   },
-      //   "apiResponseStatus": "SUCCESS"
-      // };
-
-      // runDetailsService
       var getRunDetails = function () {
         $scope.run_details.page_loader = true;
         runDetailsService.get({
@@ -148,6 +66,6 @@
         }
       };
       getRunDetails();
-      setInterval(getRunDetails, 60 * 1000);
+      setInterval(getRunDetails, 20 * 1000);
     }]);
 })();
