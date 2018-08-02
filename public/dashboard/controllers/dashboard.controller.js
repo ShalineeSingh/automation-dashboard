a1776d208eddd528570fb0238baee50d275d895e
shalineesingh <shalinee.singh@coviam.com>
2018-08-02 17:51:08 +0530

    dashboard chart integrated

--- public/dashboard/controllers/dashboard.controller.js
+++ public/dashboard/controllers/dashboard.controller.js
@@ -1,116 +1,155 @@
 (function () {
   angular.module('app.basic')
-    .controller('dashboardCtrl', ['$scope', '$rootScope', '$state', '$filter', function ($scope, $rootScope, $state, $filter) {
-      $scope.dashboard = {
-        'release_data': [{
-          'version': '1.5',
-          'web': 45,
-          'android': 56,
-          'api': 34,
-          'mobileweb': 65,
-          'ios': 89
-        }, {
-          'version': '1.5',
-          'web': 45,
-          'android': 56,
-          'api': 34,
-          'mobileweb': 65,
-          'ios': 89
-        }, {
-          'version': '1.5',
-          'web': 45,
-          'android': 56,
-          'api': 34,
-          'mobileweb': 65,
-          'ios': 89
-        }, {
-          'version': '1.5',
-          'web': 45,
-          'android': 56,
-          'api': 34,
-          'mobileweb': 65,
-          'ios': 89
-        }, {
-          'version': '1.5',
-          'web': 45,
-          'android': 56,
-          'api': 34,
-          'mobileweb': 65,
-          'ios': 89
-        }]
-      };
-      $rootScope.show_filter = false;
-
-      $scope.dashboard_colors = ["#26B99A", "#34495E", "#5a8022", "#3498DB", "#9B59B6", "#8abb6f", "#759c6a", "#bfd3b7"];
-      $scope.labels = ["Release 1", "Release 2", "Release 3", "Release 4", "Release 5"];
-      $scope.series = ['Web', 'Android', 'API', 'MobileWeb', 'iOS'];
-      $scope.data = [
-        [65, 59, 80, 81, 56],
-        [68, 48, 40, 19, 86],
-        [48, 44, 40, 49, 46],
-        [45, 76, 43, 45, 86],
-        [58, 45, 46, 45, 65]
+    .controller('dashboardCtrl', ['$scope', '$rootScope', '$state', 'dashboardService', function ($scope, $rootScope, $state, dashboardService) {
+      var graph_values = [
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null]
       ];
-      $scope.onClick = function (points, evt) {
-        console.log(points, evt);
+
+      $rootScope.show_filter = false;
+      $scope.dashboard = {
+        'chart_colors': ["#26B99A", "#34495E", "#5a8022", "#3498DB", "#9B59B6", "#8abb6f", "#759c6a", "#bfd3b7"],
+        'chart_options': {
+          'responsive': true,
+          'maintainAspectRatio': true,
+          'layout': {
+            'padding': {
+              'left': 5,
+              'right': 5,
+              'top': 5,
+              'bottom': 5
+            }
+          },
+          'tooltips': {
+            // 'mode': 'nearest',
+            'axis': 'x',
+            'xPadding': 10,
+            'yPadding': 10,
+            'backgroundColor': '#fff',
+            'borderColor': '#ddd',
+            'borderWidth': 1,
+            'titleFontColor': '#9e9e9e',
+            'titleFontSize': 12,
+            'titleFontStyle': 'normal',
+            'titleMarginBottom': 12,
+            'bodyFontColor': '#222',
+            'bodyFontSize': 12,
+            bodySpacing: 4
+          },
+          'legend': {
+            'display': true,
+            'position': 'top',
+            'labels': {
+              'usePointStyle': true,
+            }
+          },
+          scales: {
+            yAxes: [{
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            }]
+          },
+          elements: {
+            line: {
+              // fill: false,
+              //  tension: 0,
+            }
+          }
+        },
+        'platform_sequence': {
+          'API': 0,
+          'WEB': 1,
+          'ANDROID': 2,
+          'MOBILEWEB': 3,
+          'IOS': 4
+        },
+        'chart_series': ['API', 'Web', 'Android', 'MobileWeb', 'iOS']
       };
-      $scope.datasetOverride = [{
-        yAxisID: 'y-axis-1'
-      }];
-      $scope.options = {
-        'responsive': true,
-        'maintainAspectRatio': true,
-        'layout': {
-          'padding': {
-            'left': 5,
-            'right': 5,
-            'top': 5,
-            'bottom': 5
-          }
-        },
-        'tooltips': {
-          // 'mode': 'nearest',
-          'axis': 'x',
-          'xPadding': 10,
-          'yPadding': 10,
-          'backgroundColor': '#fff',
-          'borderColor': '#ddd',
-          'borderWidth': 1,
-          'titleFontColor': '#9e9e9e',
-          'titleFontSize': 12,
-          'titleFontStyle': 'normal',
-          'titleMarginBottom': 12,
-          'bodyFontColor': '#222',
-          'bodyFontSize': 12,
-          bodySpacing: 4
-        },
-        'legend': {
-          'display': true,
-          'position': 'top',
-          'labels': {
-            'usePointStyle': true,
-          }
-        },
-        scales: {
-          yAxes: [{
-            id: 'y-axis-1',
-            type: 'linear',
-            display: true,
-            position: 'left'
-          }]
-        },
-        elements: {
-          line: {
-            // fill: false,
-            //  tension: 0,
-          }
-        }
+
+      // var formatValuesForGraph = function (platform_type, platform_data_values) {
+      //   platform_data_values.forEach(function (obj, index) {
+      //     if (obj.hasOwnProperty(platform_type)) {
+      //       console.log(index);
+      //       return index;
+      //     }
+      //   });
+      // };
+
+      // var getGraphValues = function (release_array) {
+      //   var labels = [];
+      //   var series = [];
+      //   var obj_map_array = [];
+      //   var release_data_obj = {};
+      //   var platform_data_values = [];
+      //   var final_chart_values = [];
+      //   release_array.forEach(function (element) {
+      //     labels.push(element.release);
+      //     element.releaseStats.forEach(function (platform) {
+      //       var temp_platform_index;
+      //       platform_data_values.forEach(function (obj, index) {
+      //         if (obj.hasOwnProperty(platform.platformType)) {
+      //           temp_platform_index = index;
+      //         }
+      //       });
+      //       if (temp_platform_index) {
+      //         platform_data_values[temp_platform_index][platform.platformType].push(platform.passedPercentage);
+      //       } else {
+      //         obj_map_array = [];
+      //         obj_map_array.push(platform.passedPercentage);
+      //         platform_data_values.push({
+      //           [platform.platformType]: obj_map_array
+      //         });
+      //       }
+      //     });
+      //   });
+      //   // console.log(platform_data_values);
+      //   platform_data_values.forEach(function (platform, index) {
+      //     final_chart_values.push(platform[Object.keys(platform)[0]]);
+      //   });
+      // };
+      var getGraphValues = function (release_array) {
+        release_array.forEach(function (release, index) {
+          var release_index = index;
+          release.releaseStats.forEach(function (platform) {
+            if (platform.platformType === 'API')
+              graph_values[$scope.dashboard.platform_sequence.API][release_index] = platform.passedPercentage;
+            if (platform.platformType === 'WEB')
+              graph_values[$scope.dashboard.platform_sequence.WEB][release_index] = platform.passedPercentage;
+            if (platform.platformType === 'ANDROID')
+              graph_values[$scope.dashboard.platform_sequence.ANDROID][release_index] = platform.passedPercentage;
+            if (platform.platformType === 'MOBILEWEB')
+              graph_values[$scope.dashboard.platform_sequence.MOBILEWEB][release_index] = platform.passedPercentage;
+            if (platform.platformType === 'IOS')
+              graph_values[$scope.dashboard.platform_sequence.IOS][release_index] = platform.passedPercentage;
+          });
+        });
+        $scope.dashboard.chart_data = graph_values;
       };
+
+      var getDashboardData = function () {
+        $scope.dashboard.page_loader = true;
+        dashboardService.get().$promise.then(function (response) {
+          $scope.dashboard.release_data = response.responseObject;
+          console.log($scope.dashboard.release_data);
+          getGraphValues($scope.dashboard.release_data);
+        }).catch(function (error) {
+          console.log(error);
+        }).finally(function () {
+          $scope.dashboard.page_loader = false;
+        });
+      };
+
       $scope.goToState = function (release, platform) {
         $state.go('main.app.basic.platform.id', {
           'release': release,
           'platform': platform
         });
       };
+      getDashboardData();
     }]);
 })();
--- public/shared/services/automation.service.js
+++ public/shared/services/automation.service.js
@@ -1,23 +1,28 @@
 (function () {
   'use strict';
   angular.module('automation.app')
-    // .factory('testRunService', ['$resource', function ($resource) {
-    //   return $resource('http://b650fa14.ngrok.io/testrun/list');
-    // }])
-    // .factory('runDetailsService', ['$resource', function ($resource) {
-    //   return $resource('http://b650fa14.ngrok.io/testrun/:platform/:run_id');
-    // }])
-    // .factory('releaseListService', ['$resource', function ($resource) {
-    //   return $resource('http://b650fa14.ngrok.io/testrun/releases');
-    // }]);
-
     .factory('testRunService', ['$resource', function ($resource) {
-      return $resource('/public/test-run/controllers/test-runs.json');
+      return $resource('http://94ef0f81.ngrok.io/testrun/list');
     }])
     .factory('runDetailsService', ['$resource', function ($resource) {
-      return $resource('/public/test-run/controllers/run-details.json');
+      return $resource('http://94ef0f81.ngrok.io/testrun/:platform/:run_id');
     }])
     .factory('releaseListService', ['$resource', function ($resource) {
-      return $resource('/public/test-run/controllers/release-list.json');
+      return $resource('http://94ef0f81.ngrok.io/testrun/releases');
+    }])
+    .factory('dashboardService', ['$resource', function ($resource) {
+      return $resource('http://94ef0f81.ngrok.io/dashboard/history');
     }]);
+  // .factory('testRunService', ['$resource', function ($resource) {
+  //   return $resource('/public/test-run/json/test-runs.json');
+  // }])
+  // .factory('runDetailsService', ['$resource', function ($resource) {
+  //   return $resource('/public/test-run/json/run-details.json');
+  // }])
+  // .factory('releaseListService', ['$resource', function ($resource) {
+  //   return $resource('/public/test-run/json/release-list.json');
+  // }])
+  // .factory('dashboardService', ['$resource', function ($resource) {
+  //   return $resource('/public/test-run/json/dashboard.json');
+  // }]);
 })();
