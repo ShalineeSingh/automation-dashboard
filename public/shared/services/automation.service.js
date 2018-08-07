2fa82ab8f848b3a71e574c3614e4e556e9674496
shalineesingh <shalinee.singh@coviam.com>
2018-08-07 19:08:16 +0530

    loaders and alignment

--- public/shared/services/automation.service.js
+++ public/shared/services/automation.service.js
@@ -1,28 +1,28 @@
 (function () {
   'use strict';
   angular.module('automation.app')
+    // .factory('testRunService', ['$resource', function ($resource) {
+    //   return $resource('http://35a2c213.ngrok.io/testrun/list');
+    // }])
+    // .factory('runDetailsService', ['$resource', function ($resource) {
+    //   return $resource('http://35a2c213.ngrok.io/testrun/:platform/:run_id');
+    // }])
+    // .factory('releaseListService', ['$resource', function ($resource) {
+    //   return $resource('http://35a2c213.ngrok.io/testrun/releases');
+    // }])
+    // .factory('dashboardService', ['$resource', function ($resource) {
+    //   return $resource('http://35a2c213.ngrok.io/dashboard/history');
+    // }]);
     .factory('testRunService', ['$resource', function ($resource) {
-      return $resource('http://35a2c213.ngrok.io/testrun/list');
+      return $resource('/public/test-run/json/test-runs.json');
     }])
     .factory('runDetailsService', ['$resource', function ($resource) {
-      return $resource('http://35a2c213.ngrok.io/testrun/:platform/:run_id');
+      return $resource('/public/test-run/json/run-details.json');
     }])
     .factory('releaseListService', ['$resource', function ($resource) {
-      return $resource('http://35a2c213.ngrok.io/testrun/releases');
+      return $resource('/public/test-run/json/release-list.json');
     }])
     .factory('dashboardService', ['$resource', function ($resource) {
-      return $resource('http://35a2c213.ngrok.io/dashboard/history');
+      return $resource('/public/test-run/json/dashboard.json');
     }]);
-  // .factory('testRunService', ['$resource', function ($resource) {
-  //   return $resource('/public/test-run/json/test-runs.json');
-  // }])
-  // .factory('runDetailsService', ['$resource', function ($resource) {
-  //   return $resource('/public/test-run/json/run-details.json');
-  // }])
-  // .factory('releaseListService', ['$resource', function ($resource) {
-  //   return $resource('/public/test-run/json/release-list.json');
-  // }])
-  // .factory('dashboardService', ['$resource', function ($resource) {
-  //   return $resource('/public/test-run/json/dashboard.json');
-  // }]);
 })();
