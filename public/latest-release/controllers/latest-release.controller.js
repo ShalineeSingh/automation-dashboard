3e33b6b19088b7890bfab48c15df21536533cf26
shalineesingh <shalinee.singh@coviam.com>
2018-09-09 23:27:02 +0530

    tooltip and pending

--- public/latest-release/controllers/latest-release.controller.js
+++ public/latest-release/controllers/latest-release.controller.js
@@ -8,9 +8,21 @@
         'environment_list': Object.keys($rootScope.maps.env_map),
         'platform_list': Object.keys($rootScope.maps.platform_map),
       };
-      $scope.labels = ["Passed", "Failed"];
-      $scope.chart_colors = ['#1ABB9C', '#E74C3C'];
-
+      $scope.labels = ["Passed", "Failed", "Pending"];
+      $scope.chart_colors = ['#1ABB9C', '#E74C3C', '#F7F7F7'];
+      $scope.options = {
+        tooltips: {
+          enabled: true,
+          mode: 'single',
+          callbacks: {
+            label: function (tooltipItem, data) {
+              var label = data.labels[tooltipItem.index];
+              var datasetLabel = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+              return label + ': ' + datasetLabel + '%';
+            }
+          }
+        },
+      }
       $rootScope.filter_overlay = false;
       $rootScope.show_filter = false;
 
@@ -23,6 +35,8 @@
         latestReleaseService.get().$promise.then(function (response) {
           $scope.latest_release.list = response.responseObject;
           $scope.latest_release.platform_list = Object.keys($scope.latest_release.list.platformWiseBreakup);
+          $scope.latest_release.summary = response.responseObject.releaseSummary.releaseStats;
+          generateReleaseProgressBar();
           console.log($scope.latest_release.list);
           generateProgressBar();
         }).catch(function (error) {
@@ -31,14 +45,23 @@
           $scope.latest_release.page_loader = false;
         });
       };
-
+      var generateReleaseProgressBar = function () {
+        $scope.latest_release.summary.forEach(function (obj) {
+          obj.completionPercent = (((obj.failed_tests + obj.passed_tests) / obj.total_tests) * 100).toFixed(2);
+          obj.stacked = {
+            'value': [Math.round((obj.passed_tests / obj.total_tests) * 100), Math.round((obj.failed_tests / obj.total_tests) * 100)],
+            'type': ['success', 'danger']
+          };
+        });
+      };
 
       var generateProgressBar = function () {
         $scope.latest_release.platform_list.forEach(function (platform) {
           $scope.latest_release.list.platformWiseBreakup[platform].forEach(function (obj) {
             obj.completionPercent = (((obj.failed_tests + obj.passed_tests) / obj.total_tests) * 100).toFixed(2);
+            var pending = Math.round(((obj.total_tests - (obj.passed_tests + obj.failed_tests)) / obj.total_tests) * 100)
             obj.stacked = {
-              'value': [Math.round((obj.passed_tests / obj.total_tests) * 100), Math.round((obj.failed_tests / obj.total_tests) * 100)],
+              'value': [Math.round((obj.passed_tests / obj.total_tests) * 100), Math.round((obj.failed_tests / obj.total_tests) * 100), pending],
               'type': ['success', 'danger']
             };
           });
